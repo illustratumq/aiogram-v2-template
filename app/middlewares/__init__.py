313a22caa0ddee1a8fc0717c6e72783f9e34085c
illustratumq <p.mochuk-me23@lll.kpi.ua>
2023-05-13 00:39:11 +0300

    add filters, keyboards, misc

--- app/middlewares/__init__.py
+++ app/middlewares/__init__.py
@@ -1,3 +1,5 @@
+import logging
+
 from aiogram import Dispatcher
 from sqlalchemy.orm import sessionmaker
 
@@ -6,7 +8,11 @@ from app.middlewares.environment import EnvironmentMiddleware
 from app.middlewares.acl import ACLMiddleware
 
 
+log = logging.getLogger(__name__)
+
+
 def setup(dp: Dispatcher, session_pool: sessionmaker, environments: dict):
     dp.setup_middleware(EnvironmentMiddleware(environments))
     dp.setup_middleware(DatabaseMiddleware(session_pool))
     dp.setup_middleware(ACLMiddleware())
+    log.info('Мідлварі успішно встановлені...')
